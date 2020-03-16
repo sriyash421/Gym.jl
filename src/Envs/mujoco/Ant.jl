40b502aceecedf1ef0c3a4c917cab9a73ae43df1
Sry-421 <poddarsriyash@gmail.com>
2020-03-17 02:39:10 +0530

    initial ant env added

---
+++ src/Envs/mujoco/Ant.jl
@@ -0,0 +1,68 @@
+using Gym:Space
+using LyceumBase, LyceumMuJoCo, MuJoCo, Shapes
+
+mutable struct AntEnv <: AbstractMuJoCoEnvironment
+    model_path::String
+    sim::MJSim
+    observation_space::Space.Box
+    action_space::Space.Box
+    state::AbstractVecOrMat
+    skip_frame::Integer
+end
+
+function init!(n::Integer=1)
+    model_path = joinpath(@__DIR__, "ant.xml")
+    sim = MJSim(model_path, skip = n)
+    obs_high = ones((sim.m.nq - 2 + sim.m.nv + sim.m.nbody * 6,))
+    action_high = ones((sim.m.nu,))
+    observation_space = Space.Box(-obs_high, obs_high, Float64)
+    action_space = Space.Box(-action_high, action_high, Float64)
+    state = zeros((sim.m.nq+sim.m.nv,))
+    skip_frame = n
+    env = AntEnv(model_path, sim, observation_space, action_space, state, skip_frame)
+    reset!(env)
+    return env
+end
+
+function setstate(env::AntEnv)
+    temp = MultiShape(simstate = statespace(env.sim))
+    setstate!(env.sim, temp)
+    return vcat(temp.qpos,temp.qvel)
+end
+
+function getobs(env::AntEnv)
+    return vcat(env.sim.d.qpos[3:length(env.sim.d.qpos)], env.sim.d.qvel, flatten(clamp(env.sim.d.xfrc_applied,-1,1)))
+end
+
+function forward!(env::AntEnv, action)
+    env.sim.d.ctrl[:] = action
+    for _ in range(env.skip_frame)
+        step!(env.sim)
+    end
+end
+
+@inline torso_x(env::AntEnv) = env.sim.d.qpos[1]
+
+function step!(env::AntEnv, action)
+    @assert action ∈ env.action_space "$action in $(env.action_space) invalid"
+    xposbefore = torso_x(env)
+    forward!(env)
+    xposafter = torso_x(env)
+    forward_reward = (xposafter-xposbefore)/(env.skip_frame)
+    ctrl_cost = 0.5 * sum(action.^2)
+    contact_cost = 0.5 * 1e-3 * sum((clamp(env.sim.d.xfrc_applied,-1,1)).^2)
+    survive_reward = 1.0
+    reward = forward_reward - ctrl_cost - contact_cost + survive_reward
+    env.state[:] = setstate(env)
+    done = -(all(isfinite.(env.state)) && env.state[3] >=0.2 && env.state[3] <=1.0)
+    ob = getobs(env)
+    return ob, reward, done, Dict()
+end
+
+function reset!(env::AntEnv)
+    LyceumBase.reset!(env.sim)
+    # ob = getobs(env)
+    return Dict()
+end
+
+Base.show(io::IO, env::AntEnv) = print(io, "AntEnv")
